bad34833b3828bf484b107bc80cf25d82ab5c6e2
Daniel Butler <cactusfly@gmail.com>
2016-09-07 16:16:56 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//function myCode() {
+//var myCode = function() {
+//	$(".warning").hide().show("slow");
+//}
+
+//$( document ).ready(myCode);
+
+//$( document ).ready(function() {
+//	$(".warning").hide().show("slow");
+//});
+
+//$(function() {
+//	$(".warning").hide().show("slow");
+//});
+
+$(".warning").hide().show("slow");
